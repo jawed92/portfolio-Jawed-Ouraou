b4c982a228c92f3f4077a71415ee7da7b649f876
jawed92 <jawed.ouraou@gmail.com>
2023-11-02 23:53:58 +0100

    Modale Bootstrap footer contact

--- function.js
+++ function.js
@@ -1,14 +1,40 @@
-const cards = document.querySelectorAll('.card-portfolio');
+// ANIMATION TEXT BANNER	
+let title = document.getElementById("text-banner");
+let i = 0;
+let dataText = ["Bonjour !" ,"Je suis Jawed Ouraou" ,"développeur front-end"];
 
-// Parcourez chaque carte et ajoutez un gestionnaire d'événements
-cards.forEach(card => {
-  card.addEventListener('click', () => {
-    // Obtenez l'ID de la modale à partir de l'attribut data-modal-id
-    const modalId = card.getAttribute('data-modal-id');
-    // Sélectionnez la modale correspondante et affichez-la
-    const modal = document.getElementById(modalId);
-    if (modal) {
-      modal.style.display = 'block';
-    }
-  });
-});
+function typeWriter(currentText, line , j, finish) {
+
+ if (j < currentText.length) {
+   line.innerHTML += currentText[j];
+ 
+   // wait for a while and call this function again for next character
+   setTimeout(function() {
+     typeWriter(currentText, line , j + 1)
+     }, 40);
+   }
+ if(j == currentText.length) {
+   let lineText = document.getElementsByClassName("h1-content")[i];
+   lineText.classList.add("no-after");
+   i ++;
+   setTimeout(function() {
+     StartTextAnimation(i)
+   }, 100);
+   
+ }
+}
+ 
+function StartTextAnimation(i) {
+
+ if(i < dataText.length) {
+   let line = document.createElement("h1");
+   line.classList.add('h1-content');
+   title.appendChild(line);
+   let currentText = dataText[i];
+   let j = 0;
+   typeWriter(currentText ,line, j)
+ }
+}	
+
+StartTextAnimation(i);
+
